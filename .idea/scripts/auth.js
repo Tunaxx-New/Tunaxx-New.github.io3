11f99f5f684c6e98c69629c17c5c3baba13af727
Nikita Mordvintsev <90397176+Tunaxx-New@users.noreply.github.com>
2021-10-20 14:18:32 +0600

    Test

--- .idea/scripts/auth.js
+++
@@ -1,9 +0,0 @@
-var registerButton = document.getElementById("register");
-
-registerButton.addEventListener("click", event => {
-    var loginInput = document.getElementById("regLogin");
-    var PasswordInput = document.getElementById("regPassword");
-    var GroupInput = document.getElementById("regGroup");
-
-    addUser(loginInput, PasswordInput, GroupInput);
-});
