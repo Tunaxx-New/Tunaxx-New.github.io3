ab3ddec73be9f53182064e487ed6b18ad9632b09
Nikita Mordvintsev <90397176+Tunaxx-New@users.noreply.github.com>
2021-10-20 13:47:55 +0600

    Test for ICT
    site, testing fs library from node js

---
+++ .idea/scripts/fileManager.js
@@ -0,0 +1,30 @@
+var fs = require('fs');
+
+function addUser(login, password, group) {
+    fs.readFile("../users/users.json", "utf-8", (e, jsonString) => {
+        if (e) {
+            console.log("Failed to load users.json");
+        } else {
+            try {
+                data = JSON.parse(jsonString);
+
+                //...
+                const id = (data.idSize + 1);
+                data.users.push({"id": id,"login": login, "password": password,"IP": "", "group": group, "time": []});
+
+                var write = JSON.stringify(data, null, '\t')
+                write = write + '1';
+                fs.writeFile('./users.json', write, err => {
+                    if (err) {
+                        console.log('Error writing file', err)
+                    } else {
+                        console.log('Successfully wrote file')
+                    }
+                });
+            } catch(e) {
+                console.log(e);
+            }
+        }
+    });
+}
+addUser();
